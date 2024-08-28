de5cf9ccd41c9e539ce773b5e9ca944a732f15f5
Andreas Moll <port.rain@gmail.com>
2024-08-28 13:52:40 +1000

    Add super simple admin index page

--- openday_scavenger/views/admin/views.py
+++ openday_scavenger/views/admin/views.py
@@ -14,6 +14,13 @@ router = APIRouter()
 templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "static")
 
 
+@router.get("/")
+async def render_index_page(request: Request):
+    """ Render admin index page """
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
 
 
 @router.get("/puzzles")
