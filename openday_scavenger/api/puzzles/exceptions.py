a0350ee1a95d108677fa2e00a34d976570ccb306
Andreas Moll <port.rain@gmail.com>
2024-09-06 12:59:28 +1000

    Add admin page with filtering for responses

--- openday_scavenger/api/puzzles/exceptions.py
+++ openday_scavenger/api/puzzles/exceptions.py
@@ -9,3 +9,18 @@ class UnknownPuzzleError(HTTPException):
 class DisabledPuzzleError(HTTPException):
     """ Raised if the user tries to access a puzzle that has been disabled """
     pass
+
+
+class PuzzleCreationError(RuntimeError):
+    """ Raised if the creation of a new puzzle entry in the database failed """
+    pass
+
+
+class PuzzleUpdatedError(RuntimeError):
+    """ Raised if the modification of new puzzle entry in the database failed """
+    pass
+
+
+class PuzzleNotFoundError(RuntimeError):
+    """ Raised if a puzzle cannot be found in the database """
+    pass
--- openday_scavenger/views/admin/__init__.py
+++ openday_scavenger/views/admin/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from .admin import router as admin_router
 from .puzzles import router as puzzle_router
 from .visitors import router as visitor_router
+from .responses import router as response_router
 
 router = APIRouter()
 
 router.include_router(admin_router, prefix="")
 router.include_router(puzzle_router, prefix="/puzzles")
 router.include_router(visitor_router, prefix="/visitors")
+router.include_router(response_router, prefix="/responses")
