627a4a7ae790938db2c5d752bc38da330e00f91e
Stephen Mudie <stephenm@ansto.gov.au>
2024-09-06 10:31:27 +1000

    Create default puzzles path to catch unknown puzzles. Fixes #28

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -7,3 +7,8 @@ router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix='/demo')
+
+# Include a route to catch all invalid puzzle routes so we can throw a custom 404.
+@router.get("/{path:path}/")
+async def catch_all(path: str):
+    pass
