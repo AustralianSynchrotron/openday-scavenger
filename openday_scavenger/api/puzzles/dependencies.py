d65f537eab903e1cf560b831f370eca60685a90c
Andreas Moll <port.rain@gmail.com>
2024-09-09 14:35:25 +1000

    Add example how puzzles can serve local static files

--- openday_scavenger/api/puzzles/dependencies.py
+++ openday_scavenger/api/puzzles/dependencies.py
@@ -13,10 +13,12 @@ from .models import Puzzle
 async def block_disabled_puzzles(request: Request, db: Annotated["Session", Depends(get_db)]):
     """Dependency that prevents access to unknown or disabled puzzle endpoints"""
     # Use the pathlib library to deconstruct the url and find the name of the puzzle
+    # In order to allow multi-level paths, we search for the path component after the "puzzles"
+    # component in the route.
     try:
         path_parts = Path(request.url.path).parts
         puzzle_name = path_parts[path_parts.index("puzzles") + 1]
-    except ValueError:
+    except (ValueError, IndexError):
         raise UnknownPuzzleError(status_code=status.HTTP_404_NOT_FOUND, detail="Unknown Puzzle")
 
     # Look up the puzzle in the database. If the puzzle has not been registered in the database
--- openday_scavenger/puzzles/demo/views.py
+++ openday_scavenger/puzzles/demo/views.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException, status
+from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 
 from openday_scavenger.api.visitors.dependencies import get_auth_visitor
@@ -11,6 +12,23 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "static")
 
+
+@router.get("/static/{path:path}")
+async def get_static_files(
+    path: Path,
+):
+    """Serve files from a local static folder"""
+    # This route is required as the current version of FastAPI doesn't allow
+    # the mounting of folders on APIRouter. This is an open issue:
+    # https://github.com/fastapi/fastapi/discussions/9070
+    parent_path = Path(__file__).resolve().parent / "static"
+    file_path = parent_path / path
+#        ## Make sure no files outside spa_app_path are requested
+    if file_path.is_relative_to(parent_path) and file_path.is_file():
+        return FileResponse(file_path)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Requested file does not exist")
+    
 
 @router.get("/")
 async def index(
