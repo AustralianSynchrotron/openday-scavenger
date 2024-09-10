1e38eb9a7bf3b38c9f23b1382e6340416132969e
Tingting Feng <fengt@ansto.gov.au>
2024-09-10 16:19:18 +1000

    DEV: add the puzzle for counting new buildings

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
+from .newbuildings.views import router as new_buildings_router
+
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
-
+router.include_router(new_buildings_router, prefix="/newbuildings")
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
 @router.get("/{path:path}/")
