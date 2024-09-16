43f08faac795d334c5bc0dd30c54baafc633df93
Noah Kim <dongwoo_333@hanmail.net>
2024-09-17 08:03:42 +1000

    chore(element quiz): copy demo puzzle to create new puzzle for element quiz

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
+from .periodic.views import router as puzzle_periodic_router
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
+router.include_router(puzzle_periodic_router, prefix="/periodic")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
---
+++ openday_scavenger/puzzles/periodic/views.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+
+from openday_scavenger.api.visitors.dependencies import get_auth_visitor
+from openday_scavenger.api.visitors.schemas import VisitorAuth
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "static")
+
+
+@router.get("/static/{path:path}")
+async def get_static_files(
+    path: Path,
+):
+    """Serve files from a local static folder"""
+    # This route is required as the current version of FastAPI doesn't allow
+    # the mounting of folders on APIRouter. This is an open issue:
+    # https://github.com/fastapi/fastapi/discussions/9070
+    parent_path = Path(__file__).resolve().parent / "static"
+    file_path = parent_path / path
+
+    # Make sure the requested path is a file and relative to this path
+    if file_path.is_relative_to(parent_path) and file_path.is_file():
+        return FileResponse(file_path)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Requested file does not exist"
+        )
+
+
+@router.get("/")
+async def index(
+    request: Request, visitor: Annotated[VisitorAuth | None, Depends(get_auth_visitor)]
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"puzzle": "demo", "visitor": visitor.uid},
+    )
