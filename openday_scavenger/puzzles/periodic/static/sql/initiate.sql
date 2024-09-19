6ecd0c95bcadf3cf6348d01f56251b03d362f18a
Noah Kim <dongwoo_333@hanmail.net>
2024-09-19 12:31:30 +1000

    chore(element quiz): add sql queries to add element puzzles

---
+++ openday_scavenger/puzzles/periodic/static/sql/initiate.sql
@@ -0,0 +1,11 @@
+INSERT INTO puzzle
+(name, answer, active, location, notes)
+VALUES('element_general', 'Au', 1, '/element', '');
+
+INSERT INTO puzzle
+(name, answer, active, location, notes)
+VALUES('element_xas', 'Fe', 1, '/element_xas', '');
+
+INSERT INTO puzzle
+(name, answer, active, location, notes)
+VALUES('element_mex', 'Cu', 1, '/element_mex', '');
