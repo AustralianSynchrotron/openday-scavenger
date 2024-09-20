53ace1e8281a5548b7310f3dc6d5e8fccd497407
Luigi Feriani <luigi.feriani@gmail.com>
2024-09-20 22:31:12 +1000

    support multiple puzzle of the same type, split service from views, abuse of Depends

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -7,7 +7,10 @@ router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
-router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram")
+router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-probations")
+router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-crumpets")
+router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-toerags")
+router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-reboots")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
