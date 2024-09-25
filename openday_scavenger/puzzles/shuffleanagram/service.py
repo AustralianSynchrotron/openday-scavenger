c17a81fd57b89381db51f5c8cdf3bae932938ce6
Luigi Feriani <luigi.feriani@gmail.com>
2024-09-25 23:09:47 +1000

    Reusing get_puzzle_name dependency from common api

--- openday_scavenger/puzzles/shuffleanagram/service.py
+++ openday_scavenger/puzzles/shuffleanagram/service.py
@@ -1,8 +1,8 @@
 import random
-from pathlib import PurePosixPath
 
 from fastapi import Request
-from fastapi.logger import logger
+
+from openday_scavenger.api.puzzles.dependencies import get_puzzle_name
 
 PUZZLE_FAMILY = "shuffleanagram"
 
@@ -17,41 +17,23 @@ INITIAL_WORDS: dict[str, str] = {
 }
 
 
-def get_full_puzzle_name(request: Request) -> str:
-    """get_full_puzzle_name Extract the full puzzle name from the request URL:
-    it's the part of the path that starts with the PUZZLE_FAMILY
-    Should be used with Depends"""
-    try:
-        full_puzzle_name = [
-            _p for _p in PurePosixPath(request.url.path).parts if _p.startswith(PUZZLE_FAMILY)
-        ]
-        if not full_puzzle_name:
-            raise ValueError("no puzzle name found")  # we should *not* get here
-        full_puzzle_name = full_puzzle_name[0]
-
-    except Exception as e:
-        logger.error(f"error getting puzzle name from {request.url.path}: {e}; ")
-        full_puzzle_name = PUZZLE_FAMILY
-
-    return full_puzzle_name
-
-
-def get_puzzle_name(request: Request) -> str:
-    """get_puzzle_name Return the puzzle name from the full puzzle name,
+async def get_subpuzzle_name(request: Request) -> str:
+    """get_subpuzzle_name Return the puzzle name from the full puzzle name,
     e.g. shuffleanagram-probations -> probations
     Should be used with Depends"""
-    full_puzzle_name = get_full_puzzle_name(request)
-    return full_puzzle_name.split("-")[-1]
+    puzzle_name = await get_puzzle_name(request)
+    return puzzle_name.split("-")[-1]
 
 
-def get_initial_word(request: Request) -> str:
+async def get_initial_word(request: Request) -> str:
     """get_initial_word Get the initial word for the puzzle.
     Should be used with Depends"""
-    puzzle_name = get_puzzle_name(request)
-    return INITIAL_WORDS.get(puzzle_name, "PROBATIONS")
+
+    subpuzzle_name = await get_subpuzzle_name(request)
+    return INITIAL_WORDS.get(subpuzzle_name, "PROBATIONS")
 
 
-def shuffle_word(word_in: str) -> str:
+async def shuffle_word(word_in: str) -> str:
     """shuffle_word Shuffle the word"""
     word_out = "".join(random.sample(word_in, len(word_in)))
     return word_out
