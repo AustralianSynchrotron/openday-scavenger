a93afdcf786869be2e829e78ef48fb5a7453d6b1
Luigi Feriani <luigi.feriani@gmail.com>
2024-09-30 11:06:14 +1000

    tests

--- tests/api/puzzles_tests/service_test.py
+++ tests/api/puzzles_tests/service_test.py
@@ -28,3 +28,29 @@ def test_get_all(empty_db: Session) -> None:
 
     puzzles = get_all(empty_db)
     assert len(puzzles) == 1
+
+
+def test_get_all_filter(empty_db: Session) -> None:
+    # create a puzzle in the database
+    puzzle_names = ["demo", "test", "example", "foo", "bar", "baz"]
+    for _ in puzzle_names:
+        create(empty_db, puzzle_in=PuzzleCreate(name=_, answer=_, active=True))
+
+    puzzles = get_all(empty_db)
+    assert len(puzzles) == len(puzzle_names)
+
+    # filter by name one by one (querying for "start with this name" should return only one result)
+    for name in puzzle_names:
+        puzzles = get_all(empty_db, filter_by_puzzle_name=name)
+        assert len(puzzles) == 1
+
+    # add a puzzle that shares a prefix with another puzzle
+    create(empty_db, puzzle_in=PuzzleCreate(name="foo_bar", answer="foo_bar", active=True))
+    puzzles = get_all(empty_db, filter_by_puzzle_name="foo")
+    assert len(puzzles) == 2
+
+
+def test_get_all_filter_not_found(empty_db: Session) -> None:
+    # get a puzzle that doesn't exist
+    puzzles = get_all(empty_db, filter_by_puzzle_name="notfound")
+    assert len(puzzles) == 0
