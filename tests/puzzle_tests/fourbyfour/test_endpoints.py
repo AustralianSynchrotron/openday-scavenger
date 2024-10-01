701db5b32be751bafcfb4ab2038d54d8b1d66e02
Luigi Feriani <luigi.feriani@gmail.com>
2024-10-02 09:26:02 +1000

    tests

--- tests/puzzle_tests/fourbyfour/test_endpoints.py
+++ tests/puzzle_tests/fourbyfour/test_endpoints.py
@@ -1,7 +1,11 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from openday_scavenger.puzzles.fourbyfour.service import PuzzleStatus, get_parsed_solution
+from openday_scavenger.puzzles.fourbyfour.exceptions import GameOverException, PuzzleSolvedException
+from openday_scavenger.puzzles.fourbyfour.service import (
+    PuzzleStatus,
+    get_parsed_solution,
+)
 
 PREFIX = "/puzzles"
 PUZZLE_NAME = "fourbyfour"
@@ -11,14 +15,23 @@ class TestFourByFour:
     def test_index(self, mock_init_client: TestClient) -> None:
         response = mock_init_client.get(f"{PREFIX}/{PUZZLE_NAME}")
         assert response.status_code == status.HTTP_200_OK
-        assert hasattr(response, "context")
+
+    def test_index_returns_same_instance(self, mock_init_client: TestClient) -> None:
+        response = mock_init_client.get(f"{PREFIX}/{PUZZLE_NAME}")
+        assert response.status_code == status.HTTP_200_OK
+        ss = response.context["status"]  # type: ignore
+
+        response = mock_init_client.get(f"{PREFIX}/{PUZZLE_NAME}")
+        assert response.status_code == status.HTTP_200_OK
+        new_ss = response.context["status"]  # type: ignore
+
+        assert id(ss) == id(new_ss)
 
     def test_get_shuffled_words(self, mock_init_client: TestClient, mocker) -> None:
         _spy = mocker.spy(PuzzleStatus, "shuffle_words")
 
         response = mock_init_client.get(f"{PREFIX}/{PUZZLE_NAME}/shuffled")
         assert response.status_code == status.HTTP_200_OK
-        assert hasattr(response, "context")
         _spy.assert_called_once()
 
     def test_deselect_all_words(self, mock_init_client: TestClient, mocker) -> None:
@@ -26,7 +39,6 @@ class TestFourByFour:
 
         response = mock_init_client.delete(f"{PREFIX}/{PUZZLE_NAME}/selection")
         assert response.status_code == status.HTTP_200_OK
-        assert hasattr(response, "context")
         _spy.assert_called_once()
 
     def test_toggle_word_selection(self, mock_init_client: TestClient, mocker) -> None:
@@ -36,10 +48,68 @@ class TestFourByFour:
 
         response = mock_init_client.put(f"{PREFIX}/{PUZZLE_NAME}/{a_word}/selection")
         assert response.status_code == status.HTTP_200_OK
-        assert hasattr(response, "context")
-        ss = response.context["status"]
+        ss = response.context["status"]  # type: ignore
         assert ss.get_word(a_word).is_selected
         _spy.assert_called_once()
+
+    def test_toggle_word_selection_handle_exception(
+        self, mock_init_client: TestClient, mocker
+    ) -> None:
+        _spy = mocker.spy(PuzzleStatus, "toggle_word_selection")
+        # solution = get_parsed_solution()
+        # a_word = next(iter(solution[next(iter(solution.keys()))]))
+
+        response = mock_init_client.put(f"{PREFIX}/{PUZZLE_NAME}/foo/selection")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.context["message"] is not None  # type: ignore
+        # ss = response.context["status"]
+        # assert ss.get_word(a_word).is_selected
+        _spy.assert_called_once()
+
+    def test_reset(self, mock_init_client: TestClient, mocker) -> None:
+        # first get the status, then reset it and check id is different
+        response = mock_init_client.get(f"{PREFIX}/{PUZZLE_NAME}")
+        assert response.status_code == status.HTTP_200_OK
+        ss = response.context["status"]  # type: ignore
+
+        response = mock_init_client.delete(f"{PREFIX}/{PUZZLE_NAME}/")
+        assert response.status_code == status.HTTP_200_OK
+        new_ss = response.context["status"]  # type: ignore
+        assert new_ss.n_selected_words == 0
+        assert len(new_ss.words) == len(new_ss.categories) * new_ss.selectable_at_once
+
+        assert id(ss) != id(new_ss)
+
+    def test_submit_selection_game_over(self, mock_init_client: TestClient, mocker) -> None:
+        mocker.patch(
+            "openday_scavenger.puzzles.fourbyfour.service.PuzzleStatus.submit_selection",
+            side_effect=GameOverException,
+        )
+        response = mock_init_client.post(f"{PREFIX}/{PUZZLE_NAME}/selection-submission")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.context["game_over"] is True  # type: ignore
+        assert response.context["message"] is not None  # type: ignore
+
+    def test_submit_selection_puzzle_solved(self, mock_init_client: TestClient, mocker) -> None:
+        mocker.patch(
+            "openday_scavenger.puzzles.fourbyfour.service.PuzzleStatus.submit_selection",
+            side_effect=PuzzleSolvedException,
+        )
+        response = mock_init_client.post(f"{PREFIX}/{PUZZLE_NAME}/selection-submission")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.context["register_success"] is True  # type: ignore
+        assert response.context["message"] is not None  # type: ignore
+
+    def test_submit_selection_exception(self, mock_init_client: TestClient, mocker) -> None:
+        mocker.patch(
+            "openday_scavenger.puzzles.fourbyfour.service.PuzzleStatus.submit_selection",
+            side_effect=Exception,
+        )
+        response = mock_init_client.post(f"{PREFIX}/{PUZZLE_NAME}/selection-submission")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.context["register_success"] is False  # type: ignore
+        assert response.context["game_over"] is False  # type: ignore
+        assert response.context["message"] is not None  # type: ignore
 
     def test_get_static_files_parametrized(
         self,
