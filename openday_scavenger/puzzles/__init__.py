cd558f5df36d9418436126e175267f5a93e339c2
Mel Makin <makinm@ansto.gov.au>
2024-10-07 10:06:35 +1100

    add puzzle xray filters

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
+from .xray_filters.views import router as puzzle_xray_filters_router
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
+router.include_router(puzzle_xray_filters_router, prefix="/xray_filters")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
