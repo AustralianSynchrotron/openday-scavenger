8b2fe25cdca8baed5cc69683752e0161457a5f94
Andreas Moll <port.rain@gmail.com>
2024-10-07 11:51:26 +1100

    Restructure admin CSS files and imports

--- openday_scavenger/views/admin/puzzles.py
+++ openday_scavenger/views/admin/puzzles.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request
-from fastapi.responses import StreamingResponse
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.responses import FileResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
@@ -19,7 +19,27 @@ from openday_scavenger.config import get_settings
 
 router = APIRouter()
 config = get_settings()
-templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "static")
+templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "templates")
+
+
+@router.get("/static/{path:path}")
+async def get_static_files(
+    path: Path,
+):
+    """Serve files from a local static folder"""
+    # This route is required as the current version of FastAPI doesn't allow
+    # the mounting of folders on APIRouter. This is an open issue:
+    # https://github.com/fastapi/fastapi/discussions/9070
+    parent_path = Path(__file__).resolve().parent / "static"
+    file_path = parent_path / path
+
+    # Make sure the requested path is a file and relative to this path
+    if file_path.is_relative_to(parent_path) and file_path.is_file():
+        return FileResponse(file_path)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Requested file does not exist"
+        )
 
 
 @router.get("/")
--- openday_scavenger/views/admin/responses.py
+++ openday_scavenger/views/admin/responses.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Form, Request
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
+from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
@@ -11,7 +12,27 @@ from openday_scavenger.api.puzzles.service import generate_test_data, get_all_re
 
 router = APIRouter()
 
-templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "static")
+templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "templates")
+
+
+@router.get("/static/{path:path}")
+async def get_static_files(
+    path: Path,
+):
+    """Serve files from a local static folder"""
+    # This route is required as the current version of FastAPI doesn't allow
+    # the mounting of folders on APIRouter. This is an open issue:
+    # https://github.com/fastapi/fastapi/discussions/9070
+    parent_path = Path(__file__).resolve().parent / "static"
+    file_path = parent_path / path
+
+    # Make sure the requested path is a file and relative to this path
+    if file_path.is_relative_to(parent_path) and file_path.is_file():
+        return FileResponse(file_path)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Requested file does not exist"
+        )
 
 
 @router.get("/")
