4f60fef6545a7901ae423eadcb916548f4501502
Michelle Chan <chanm@ansto.gov.au>
2024-10-07 18:45:18 +1100

    Public map display page

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
+from .map.views import router as map_router
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
+router.include_router(map_router, prefix="/map")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
