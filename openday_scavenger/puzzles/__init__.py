5bc21e5a83a6f737d5dc07facc2801c3d418a985
Mel Makin <makinm@ansto.gov.au>
2024-10-08 09:54:18 +1100

    added new puzzle router

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
+from .ads_question_answer_matchup.views import router as ads_question_answer_matchup_router
 from .demo.views import router as puzzle_demo_router
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
+router.include_router(ads_question_answer_matchup_router, prefix="/ads_question_answer_matchup")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
