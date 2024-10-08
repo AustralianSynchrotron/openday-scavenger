80d53eac3bb0727ad691277addb09c4a9ca0470f
Gary Ruben <rubeng@ansto.gov.au>
2024-10-09 10:27:32 +1100

    interim checkin

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
+from .imagereveal.views import router as puzzle_imagereveal_router
 
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
+router.include_router(puzzle_imagereveal_router, prefix="/imagereveal")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
