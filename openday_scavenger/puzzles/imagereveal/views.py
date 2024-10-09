515b1d35d342a8e579312caa5903c653796d9a20
Gary Ruben <rubeng@ansto.gov.au>
2024-10-09 16:25:18 +1100

    intermediate checkin

--- openday_scavenger/puzzles/imagereveal/views.py
+++ openday_scavenger/puzzles/imagereveal/views.py
@@ -1,15 +1,22 @@
 from pathlib import Path
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 
+from openday_scavenger.api.db import get_db
+from openday_scavenger.api.puzzles.service import get_puzzle_state, set_puzzle_state
 from openday_scavenger.api.visitors.dependencies import get_auth_visitor
 from openday_scavenger.api.visitors.schemas import VisitorAuth
 
-router = APIRouter()
+PUZZLE_NAME = "imagereveal"
+ANIMALS = [3, 5, 6, 1]
+FRACTIONS = [1, 5, 9, 10]
+INITIAL_GUESSES = 6
 
+router = APIRouter()
 templates = Jinja2Templates(directory=Path(__file__).resolve().parent / "static")
 
 
@@ -34,9 +41,94 @@ async def get_static_files(
 
 
 @router.get("/")
-async def index(request: Request, visitor: Annotated[VisitorAuth, Depends(get_auth_visitor)]):
+async def index(
+    request: Request,
+    db: Annotated["Session", Depends(get_db)],
+    visitor: Annotated[VisitorAuth, Depends(get_auth_visitor)],
+):
+    # We demonstrate the use of state by incrementing a counter each time a user
+    # access this puzzle endpoint.
+    # Use this to store any intermediate state of the visitor while completing a puzzle.
+    state = get_puzzle_state(db, puzzle_name=PUZZLE_NAME, visitor_auth=visitor)
+    state["complete"] = False
+    state["state_access_count"] = state.get("state_access_count", 0) + 1
+    state["animal_id"] = state.get("animal_id", 1)
+    state["fraction"] = state.get("fraction", FRACTIONS[0])
+    set_puzzle_state(db, puzzle_name=PUZZLE_NAME, visitor_auth=visitor, state=state)
+
+    # Render the puzzle game page
     return templates.TemplateResponse(
         request=request,
         name="index.html",
-        context={"puzzle": "imagereveal", "visitor": visitor.uid},
+        context={"puzzle": PUZZLE_NAME, "state": state},
     )
+
+
+@router.post("/partsubmission")
+async def partsubmission(
+    animal: Annotated[str, Form()],
+    request: Request,
+    db: Annotated["Session", Depends(get_db)],
+    visitor: Annotated[VisitorAuth, Depends(get_auth_visitor)],
+):
+    state = get_puzzle_state(db, puzzle_name=PUZZLE_NAME, visitor_auth=visitor)
+    print("animal", animal)
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"puzzle": PUZZLE_NAME, "state": state},
+    )
+
+
+# @router.post("/partsubmission")
+# async def partsubmission(
+#     animal: Annotated[str, Form()],
+#     request: Request,
+#     db: Annotated["Session", Depends(get_db)],
+#     visitor: Annotated[VisitorAuth, Depends(get_auth_visitor)],
+# ):
+#     # We demonstrate the use of state by incrementing a counter each time a user
+#     # access this puzzle endpoint.
+#     # Use this to store any intermediate state of the visitor while completing a puzzle.
+#     state = get_puzzle_state(db, puzzle_name=PUZZLE_NAME, visitor_auth=visitor)
+#     animal_id = state.get("animal_id")
+#     if animal == animal_id:
+#         # correct guess
+#         state["correct_guesses"] = state.get("correct_guesses", 0) + 1
+#         state["remaining_guesses"] = state.get("remaining_guesses", INITIAL_GUESSES)
+#         state["animal_id"] = state.get("animal_id", 1) + 1
+#         state["fraction"] = FRACTIONS[0]
+
+#             # Render the puzzle game page
+#         return templates.TemplateResponse(
+#             request=request,
+#             name="inter.html",
+#             context={"puzzle": PUZZLE_NAME, "state": state},
+#         )
+#     else:
+#         # incorrect guess
+#         pass
+#         # state["correct_guesses"] = state.get("correct_guesses", 0)
+#         # state["remaining_guesses"] = state.get("remaining_guesses", INITIAL_GUESSES) - 1
+#         # if state["remaining_guesses"] < 1:
+#         #     # failed to solve puzzle
+#         #     # Do something if there are no remaining guesses
+#         #     pass
+#         # fraction = state.get("fraction", 1)
+#         # if fraction < FRACTIONS[-1]:
+#         #     next_ix = FRACTIONS.index(fraction) + 1
+#         #     fraction = FRACTIONS[next_ix]
+#         # else:
+#         #     state["animal_id"] = state.get("animal_id", 1)
+#         # state["fraction"] = fraction
+#     set_puzzle_state(db, puzzle_name=PUZZLE_NAME, visitor_auth=visitor, state=state)
+
+#     # Do something if all animals guessed correctly
+
+
+#     # Render the puzzle game page
+#     return templates.TemplateResponse(
+#         request=request,
+#         name="index.html",
+#         context={"puzzle": PUZZLE_NAME, "state": state},
+#     )
