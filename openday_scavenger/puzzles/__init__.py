5082cb5df452503fdabe1785bb440e83368d3144
Tingting Feng <fengt@ansto.gov.au>
2024-10-09 14:11:27 +1100

    DEV: add puzzle ant

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
 from .demo.views import router as puzzle_demo_router
-
+from .ant.views import router as ant_puzzle
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
 router.include_router(puzzle_demo_router, prefix="/demo")
-
+router.include_router(ant_puzzle, prefix="/ant")
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
 @router.get("/{path:path}/")
