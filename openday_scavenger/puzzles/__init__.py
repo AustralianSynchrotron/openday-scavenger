89cc09031d669e041f5a9a57e9b7afd52db0b8b1
Mel Makin <makinm@ansto.gov.au>
2024-10-10 09:15:41 +1100

    fix merge conflict

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .ads_question_answer_matchup.views import router as ads_question_answer_matchup_router
 from .cube.views import router as puzzle_cube_router
 from .demo.views import router as puzzle_demo_router
 from .shuffleanagram.views import router as puzzle_shuffleanagram_router
@@ -13,6 +14,7 @@ router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-prob
 router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-crumpets")
 router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-toerags")
 router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-reboots")
+router.include_router(ads_question_answer_matchup_router, prefix="/ads_question_answer_matchup")
 
 
 # Include a route to catch all invalid puzzle routes so we can throw a custom 404.
