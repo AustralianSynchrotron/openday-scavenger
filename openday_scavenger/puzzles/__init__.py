3b0ee9d8e2b276ff642f1dfb78f5f1d0684467b1
Tingting Feng <fengt@ansto.gov.au>
2024-10-10 12:00:09 +1100

    DEV: update import

--- openday_scavenger/puzzles/__init__.py
+++ openday_scavenger/puzzles/__init__.py
@@ -8,8 +8,8 @@ from .shuffleanagram.views import router as puzzle_shuffleanagram_router
 router = APIRouter()
 
 # Include puzzle routes. Name entered into database should match the prefix.
-router.include_router(puzzle_demo_router, prefix="/demo")
 router.include_router(puzzle_cube_router, prefix="/cube")
+router.include_router(puzzle_demo_router, prefix="/demo")
 router.include_router(new_buildings_router, prefix="/newbuildings")
 router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-probations")
 router.include_router(puzzle_shuffleanagram_router, prefix="/shuffleanagram-crumpets")
